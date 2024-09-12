d4ef2e897039ad99729125461713e132972b4e7a
Souwg <sansanvzla@gmail.com>
2024-09-12 04:30:39 +0000

    poker_card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const letras = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+const iconos = ["♦", "♥", "♠", "♣"];
+
+function randomletra() {
+  const indiceAleatorio = Math.floor(Math.random() * letras.length);
+  return letras[indiceAleatorio];
+}
+
+function randomIcono() {
+  const iconos = ["♦", "♥", "♠", "♣"];
+  const iconoAleatorio = Math.floor(Math.random() * iconos.length);
+  const icono = iconos[iconoAleatorio];
+  if (icono === "♥" || icono === "♦") {
+    return `<span class="red">${icono}</span>`;
+  } else {
+    return icono;
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let letterNumber1 = document.querySelector("#letterNUMBER");
+  letterNumber1.innerHTML = randomletra();
+  let iconorow1 = document.querySelector("#iconos2");
+  let igualarAmbosIconos = (iconorow1.innerHTML = randomIcono());
+  let iconorow2 = document.querySelector("#iconos1");
+  iconorow2.innerHTML = igualarAmbosIconos;
 };
